d19fccadeccccba1e6cc4017d8a80c7e31b2b6db
Eduardo <edu2andrade@gmail.com>
2022-12-24 08:05:18 +0100

    TailwindCSS implemented

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,8 +16,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={hGrotesk.className}>
-        <h1>Hello World</h1>
-        <p>This  font weight should be 400</p>
+        <h1 className='text-3xl font-bold '>Hello World</h1>
+        <p>This font weight should be 400</p>
       </main>
     </>
   )
